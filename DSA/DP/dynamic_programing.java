e41cd15e1225666b15cc398de73ec80acc36140d
Tanishq <tanishqlokhande2005@gmail.com>
2025-02-22 18:59:40 +0530

    initial commit

--- DSA/DP/dynamic_programing.java
+++ DSA/DP/dynamic_programing.java
@@ -453,6 +453,62 @@ public class dynamic_programing {
         return dp[n][m];
     }
 
+    // Topic: Catalan's Number
+    // Info: Recursion
+    public static int catalansnumber1(int n) {
+        if (n == 0 || n == 1) {
+            return 1;
+        }
+        int ans = 0;
+        for (int i = 0; i <= n - 1; i++) {
+            ans += catalansnumber1(i) * catalansnumber1(n - i - 1);
+        }
+        return ans;
+    }
+
+    // Info: Memoization
+    public static int catalansnumber2(int n, int[] dp) {
+        if (n == 0 || n == 1) {
+            return 1;
+        }
+        if (dp[n] != -1) {
+            return dp[n];
+        }
+        int ans = 0;
+        for (int i = 0; i < n; i++) {
+            ans += catalansnumber2(i, dp) * catalansnumber2(n - i - 1, dp);
+        }
+        return dp[n] = ans;
+    }
+
+    // Info: Tabulation
+    public static int catalansnumber3(int n) {
+        int[] dp = new int[n + 1];
+        dp[0] = dp[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            for (int j = 0; j < i; j++) {
+                dp[i] += dp[j] * dp[i - j - 1];
+            }
+        }
+        return dp[n];
+    }
+
+    // Topic: Count BST
+    public static int countBST(int n) {
+        int[] dp = new int[n + 1];
+        dp[0] = dp[1] = 1;
+        for (int i = 2; i < n + 1; i++) {
+            // Info: Ci -> BST (i Nodes) -> dp[i]
+            for (int j = 0; j < i; j++) {
+                // Info: Ci = Cj * Ci-Cj-1
+                int left = dp[j];
+                int right = dp[i - j - 1];
+                dp[i] += left * right;
+            }
+        }
+        return dp[n];
+    }
+
     public static void main(String[] args) {
         // int n = 5;
         // Info: Memoization 
@@ -535,8 +591,21 @@ public class dynamic_programing {
         String str2 = "pea";
         System.out.println(stringConversion(str1, str2)); */
         // Topic: Wildcard Matching
-        String s = "abc";
+        /* String s = "abc";
         String p = "**d";
-        System.out.println(WildcardMatching(s, p));
+        System.out.println(WildcardMatching(s, p)); */
+        // Topic: Catalan's Number
+        // Info: Recursion
+        /* int n = 4;
+        System.out.println(catalansnumber1(n)); */
+        // Info: Memoization
+        /* int[] dp = new int[n+1];
+        Arrays.fill(dp, -1);
+        System.out.println(catalansnumber2(n, dp)); */
+        // Info: Tabulation
+        // System.out.println(catalansnumber3(n));
+        // Topic: Count BST
+        System.out.println(countBST(5));
+        
     }
 }
