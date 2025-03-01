a19a30cac85dd27f9f0de9b27e804735ab8b68d3
Tanishq <tanishqlokhande2005@gmail.com>
2025-03-01 21:31:21 +0530

    Initial Commit

--- DSA/Segment Trees/Segment_trees.java
+++ DSA/Segment Trees/Segment_trees.java
@@ -1,3 +1,24 @@
 public class Segment_trees {
-    
+    // Topic: Creation of Segment Tree
+    static int tree[];
+
+    public static void Init(int n) {
+        tree = new int[4 * n];
+    }
+
+    // Info: Build
+    public static int buildST(int arr[], int sti, int start, int end) {
+        if (start == end) {
+            tree[sti] = arr[start];
+            return arr[start];
+        }
+        int mid = (start + end) / 2;
+        int left = buildST(arr, 2 * sti + 1, start, mid);
+        int right = buildST(arr, 2 * sti + 2, mid + 1, end);
+        return tree[sti] = left + right;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Segment Trees");
+    }
 }
